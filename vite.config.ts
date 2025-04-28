8c377a42b3b750fae45bcedec6c961b4b29f2a29
merhan-mohamed <merhan_mohamed89@hotmail.com>
2025-04-28 11:16:52 +0200

    update7

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"Sorting-and-Filtering-/"
+  base:"Sorting-and-Filtering"
 })
